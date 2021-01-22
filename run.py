8b7359950cd1370cdb247488b3c34fdf5fd0c5a6
ricardo-f <53285903+ricardo-f@users.noreply.github.com>
2021-01-21 22:15:07 -0300

    Updating tag value

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ client = session.client('ec2')
 #response = webhook.execute()
 
 def describe_and_count():
-    k8s_ec2 = client.describe_instances(Filters=[{'Name': 'tag:Name', 'Values': ['CE-POC']}])
+    k8s_ec2 = client.describe_instances(Filters=[{'Name': 'tag:k8s.io/cluster-autoscaler/ekslab01', 'Values': ['owned']}])
     instance_count = len(k8s_ec2['Reservations']) - 1
     gen_random_index = random.randrange(0, instance_count)
     target = k8s_ec2['Reservations'][gen_random_index]['Instances'][0]['InstanceId']
